9aabc256ebc2e8338a3f8b9fc5687d572d7da32a
Xochitl Menjivar <xandra.menji@gmail.com>
2018-01-16 22:16:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 
 function updateObjectsWithKeyAndValue(object, key, value){
-  return Object.assign({}, object, {[key] : [value]});
+  return Object.assign({}, object, {key: value});
 }
